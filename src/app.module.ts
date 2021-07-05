7a23eed7f70679cd760107bbc7361629352c6c8b
Furkan Arslan <frkn__asl@windowslive.com>
2021-07-05 14:09:23 +0300

    add typeorm db connection to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'furkana',
+      password: '0071',
+      database: 'gym',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
