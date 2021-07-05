ef1675af1e8bdf24caccc7566d8cfab7407f4e51
Furkan Arslan <frkn__asl@windowslive.com>
2021-07-05 14:06:29 +0300

    set the swagger document and validation pipe on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('GymAPI')
+    .setDescription('Documentation of the Gym API')
+    .setVersion('1.0.0')
+    .addTag('gym')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
