4b168fe313b459a0fd99131658fc9e0ceeaca0de
Furkan Arslan <frkn__asl@windowslive.com>
2021-07-18 04:54:57 +0300

    add modules to main appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { MemberModule } from './member/member.module';
+import { MembershipModule } from './membership/membership.module';
+import { ScheduleModule } from '@nestjs/schedule';
 import { EventModule } from './event/event.module';
+import { WorkoutModule } from './workout/workout.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
+  imports: [
     ConfigModule.forRoot(),
     UserModule,
     AuthModule,
+    MemberModule,
+    MembershipModule,
+    WorkoutModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
+        type: 'postgres',
         host: configService.get('POSTGRES_HOST'),
         port: +configService.get<number>('POSTGRES_HOST'),
         username: configService.get('POSTGRES_USER'),
@@ -21,12 +29,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         database: configService.get('POSTGRES_DB'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
-    }),
+      }),
       inject: [ConfigService],
     }),
     ScheduleModule.forRoot(),
     EventModule,
+    WorkoutModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
