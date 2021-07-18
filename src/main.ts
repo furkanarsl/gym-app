f5cf8b9275eaeb1bbd1116d5be98751e3989d2ec
Furkan Arslan <frkn__asl@windowslive.com>
2021-07-18 04:55:31 +0300

    enable cors enable validation pipe transformation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    exposedHeaders: ['content-range'],
+  });
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('GymAPI')
@@ -15,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
 bootstrap();
