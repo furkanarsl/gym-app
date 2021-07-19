e44017d61791617eb18b345861870f636f61e2c0
Furkan Arslan <frkn__asl@windowslive.com>
2021-07-19 04:16:22 +0300

    workout dto

--- src/workout/dto/create-workout.dto.ts
+++ src/workout/dto/create-workout.dto.ts
@@ -1,1 +1,10 @@
-export class CreateWorkoutDto {}
+import { IsArray, IsString } from 'class-validator';
+import { Routine } from '../entities/routine.entity';
+
+export class CreateWorkoutDto {
+  @IsString()
+  name: string;
+
+  @IsArray()
+  routines: Routine[];
+}
